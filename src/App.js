f4e9bb734d804fc0efdc9aad04cde56777c0cca8
Yonatan Haddish <yonihailshn@gmail.com>
2021-10-22 14:39:12 -0400

    forecast-weather section structured

--- src/App.js
+++ src/App.js
@@ -15,7 +15,34 @@ function App() {
           <input type="text" className="search-bar" placeholder="Search..."></input>
         </div>
         <div className="weather-box">
-          <h2>Hello World!</h2>
+          <div className="current-weather">
+            <div className="location">Toronto, CA</div>
+            <div className="date">Friday 22 October 2021</div>
+            <div className="temprature">6°c</div>
+            <div className="status">Cloudy</div>
+          </div>
+          <div className="forecast-weather">
+            <div className="day" id="day01">
+              <div className="date01">Oct 23</div>
+              <div className="temp01">6°c</div>
+            </div>
+            <div className="day" id="day02">
+              <div className="date02">Oct 24</div>
+              <div className="temp02">5°c</div>
+            </div>
+            <div className="day" id="day03">
+              <div className="date01">Oct 25</div>
+              <div className="temp01">16°c</div>
+            </div>
+            <div className="day" id="day04">
+              <div className="date01">Oct 26</div>
+              <div className="temp01">9°c</div>
+            </div>
+            <div className="day" id="day05">
+              <div className="date01">Oct 27</div>
+              <div className="temp01">5°c</div>
+            </div>
+          </div>
         </div>
       </main>
     </div>
